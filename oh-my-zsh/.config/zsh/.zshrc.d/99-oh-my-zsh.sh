26dd7b55c99fecf396219e8603ae490ad15f25eb
Stephen Cox <andronics@gmail.com>
2024-09-06 00:34:12 +0100

    core apps

---
+++ oh-my-zsh/.config/zsh/.zshrc.d/99-oh-my-zsh.sh
@@ -0,0 +1,16 @@
+plugins=(
+  almostontop
+  autoupdate
+  docker
+  emoji
+  git
+  kubectl
+  node
+  sudo
+  yarn
+  zsh-syntax-highlighting
+  zsh-256color
+  zsh-abbr
+)
+
+# source ${ZSH}/oh-my-zsh.sh
